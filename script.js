7e611d8619bc3b41ae075f7126994248fa7df9bc
nika-talent-solutions <socialprofilehandler@gmail.com>
2026-07-09 07:28:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
 
+menuBtn.addEventListener("click", () => {
+
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none";
+    } else {
+        mobileMenu.style.display = "block";
+    }
+
+});
