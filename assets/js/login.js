ebc1d8da7892e43d04f982374d97f3a13493cb4a
nika-talent-solutions <socialprofilehandler@gmail.com>
2026-07-09 13:15:58 +0530

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,34 @@
- 
+ import { auth } from "./firebase.js";
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const googleBtn = document.getElementById("googleLogin");
+
+if (googleBtn) {
+
+  googleBtn.addEventListener("click", async () => {
+
+    const provider = new GoogleAuthProvider();
+
+    try {
+
+      const result = await signInWithPopup(auth, provider);
+
+      alert("Welcome " + result.user.displayName + " 🎉");
+
+      window.location.href = "profile.html";
+
+    } catch (error) {
+
+      alert(error.message);
+
+      console.error(error);
+
+    }
+
+  });
+
+}
