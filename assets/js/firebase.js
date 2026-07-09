a60592beff095f815d8b15439a406d0c9782bf23
nika-talent-solutions <socialprofilehandler@gmail.com>
2026-07-09 12:37:46 +0530

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,1 +1,50 @@
- 
+ // =====================================================
+// Firebase Configuration
+// NIKA Talent Solutions
+// =====================================================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCLA5I1szr2pMObKkuDUhEMWjNk5pi5lYQ",
+
+  authDomain: "nika-talent-solutions.firebaseapp.com",
+
+  projectId: "nika-talent-solutions",
+
+  storageBucket: "nika-talent-solutions.firebasestorage.app",
+
+  messagingSenderId: "751148737305",
+
+  appId: "1:751148737305:web:26b16c54974f0339c45b88",
+
+  measurementId: "G-WNRF7MRB3H"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+export {
+
+app,
+
+auth,
+
+db,
+
+storage
+
+};
