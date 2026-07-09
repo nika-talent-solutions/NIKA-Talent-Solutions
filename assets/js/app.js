5204f0494c77f0b62653d249cd6df8c402dc4739
nika-talent-solutions <socialprofilehandler@gmail.com>
2026-07-09 09:21:07 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
- 
+ // =========================
+// Mobile Menu
+// =========================
+
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if(menuBtn){
+
+menuBtn.addEventListener("click",()=>{
+
+mobileMenu.classList.toggle("show");
+
+});
+
+}
